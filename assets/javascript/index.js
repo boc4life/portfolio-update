c1f4409cb4ffff131b94738a9e6c3b9c4f7c53bb
boc4life <lheandrew@gmail.com>
2019-02-19 14:39:07 -0500

    portfolio skeleton test

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,17 +1,21 @@
-var landing = new Vue({
-    el: '#landingWelcome',
-    data: {
-        show: true,
-        name: "Andrew Finkernagel",
-        desc: "Ambitious Full-Stack Web Developer",
-        attribute: "View My Feature Work",
-        card: "",
-        cardbody: ""
-    },
-    methods: {
-        fadeout: function() {
-            landing._data.show = false;
-        }
-    }
-  })
+// var landing = new Vue({
+//     el: '#landingWelcome',
+//     data: {
+//         show: true,
+//         name: "Andrew Finkernagel",
+//         desc: "Ambitious Full-Stack Web Developer",
+//         attribute: "View My Feature Work",
+//         card: "",
+//         cardbody: "Test1",
+//         card2body: "Test2",
+//         card3body: "Test3",
+//         card4body: "Test4"
+//     },
+//     methods: {
+//         fadeout: function() {
+//             landing._data.show = false;
+//             $("#landingWelcome").css("flex-direction", "row")
+//         }
+//     }
+//   })
 
